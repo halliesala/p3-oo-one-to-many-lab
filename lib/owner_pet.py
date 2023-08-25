d7f08649ebb43d7941217bd91edd4fc19c50cc21
Hallie Sala <halliesala19@gmail.com>
2023-08-25 14:33:01 -0400

    complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,41 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+
+        self.pet_type = pet_type
+
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, value):
+        if value in Pet.PET_TYPES:
+            self._pet_type = value
+        else:
+            raise Exception("Invalid pet type")
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def get_sorted_pets(self):
+        pet_list = self.pets()
+        pet_list.sort(key=lambda pet : pet.name)
+        return pet_list
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception('Argument must be Pet object')
